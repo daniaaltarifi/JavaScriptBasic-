c8dae2a49abb40ea6c4855630b27411053d8d274
daniaaltarifi <dania.a.altarifi@gmail.com>
2023-04-26 15:43:56 +0300

    javaScript Basic

---
+++ script.js
@@ -0,0 +1,70 @@
+var cash = 1000;
+var current =500;
+console.log(cash / current);
+var revenue = 1000; 
+var expenses = 500;
+console.log(revenue - expenses);
+var liabilites = 1000;
+var equity = 500;
+console.log(liabilites + equity);
+var profit = 1000;
+var sales = 500;
+console.log(profit * sales);
+var num = 7;
+var num1 = 9;
+var num2=2;
+console.log((num + num1 + num2) /3);
+var price = 150;
+var discount = 30/100;
+var total = price - price * discount;
+console.log(total);
+
+let age=20;
+if (age >=18 && age <=30){
+console.log("true")
+}
+else{
+    console.log("false");
+}
+var number;
+console.log(2 ** 3);
+var reminder;
+console.log(2 % 4);
+// question 2
+console.log(typeof(100));
+console.log(typeof(73.9));
+console.log(typeof(NaN));
+console.log(typeof("water"))
+console.log(typeof(false))
+console.log(typeof(9 != 11))
+console.log( "Orang" + "e")
+console.log( "Orange" - "s")
+console.log( "4" + "8")
+console.log( "4" - "8")
+console.log( "name" + 3)
+console.log( "name" - 3)
+console.log( 82 * "word")
+console.log( 1 + "hello")
+console.log( "hello" + 1)
+console.log( 1 + true)
+console.log( "hello" + true)
+console.log( typeof (Infinity))
+console.log( 1 == '1')
+console.log(1 === '1')
+
+
+
+
+// question 3
+var orange = "welcome to orange";
+var jordan="jordan";
+console.log(orange.toUpperCase());
+console.log(orange.replace("welcome","Hello")&&("from","TO"))
+console.log(orange.toLowerCase())
+console.log(orange.concat(" " , jordan))
+console.log(orange.length);
+console.log(orange.replaceAll('welcome','Hello')
+.replaceAll('to','from'));
+console.log(orange.replace("orange"," “orange“ "))
+var str = "cactus";
+console.log(str.slice("c","c"), str.replace("c" , "*"));
